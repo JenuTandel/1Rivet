14e103976fce9b25e20e417f087d8dabbf37d437
Jinal Tandel <jinaltandel06@gmail.com>
2022-09-13 17:59:04 +0530

    Two way binding

--- Task/11-09/src/app/app.component.ts
+++ Task/11-09/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Task1';
+
+  // size:number = 16;
 }
--- Task/11-09/src/app/home/home.component.ts
+++ Task/11-09/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // @Input() size:number = 0;
   fsize:number;
   btntype:string = 'increase';
 
@@ -28,6 +29,6 @@ export class HomeComponent implements OnInit {
     // else{
     //   this.fsize = this.fsize - 2;
     // }
-    
+
   }
 }
