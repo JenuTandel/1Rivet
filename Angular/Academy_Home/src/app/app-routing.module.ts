a72419577f96720f94705fec3a5b87377961b9b6
Jinal Tandel <jinaltandel06@gmail.com>
2022-10-06 19:46:45 +0530

    Implement home and aboutus

--- Angular/Academy_Home/src/app/app-routing.module.ts
+++ Angular/Academy_Home/src/app/app-routing.module.ts
@@ -1,21 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './core/authentication/login/login.component';
+import { RegistrationComponent } from './core/authentication/registration/registration.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path:'',
-    pathMatch:'full',
-    redirectTo:'home'
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
   },
   {
-    path:'home',
-    component:HomeComponent
+    path: 'home',
+    component: HomeComponent
   },
   {
-    path:'contact',
+    path: 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutusComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
